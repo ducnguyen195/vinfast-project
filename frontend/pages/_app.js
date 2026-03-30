d522e953843a11c5c278e86df8d079ea024d12c0
Duc Nguyen <duca23vietnam@gmail.com>
2026-03-30 17:17:52 +0700

    fix reponsive mobile

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,23 @@
 import '../src/index.css';
+import Head from 'next/head';
 import Navbar from '../src/components/Navbar';
 import Footer from '../src/components/Footer';
 import FloatingContact from '../src/components/FloatingContact';
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
-      <Navbar />
-      <main className="flex-grow pb-24">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-      <FloatingContact />
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </Head>
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="flex-grow pb-24">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+        <FloatingContact />
+      </div>
+    </>
   );
 }
